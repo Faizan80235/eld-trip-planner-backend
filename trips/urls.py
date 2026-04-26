b73aae4955b8399954d6a5b62c13742a9fee4bc9
Faizan80235 <fa3031980@gmail.com>
2026-04-27 04:02:04 +0500

    initial configuration and settings

--- trips/urls.py
+++ trips/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
     path('calculate-route/', views.calculate_route, name='calculate_route'),
-    path('health/', views.health_check, name='health_check'),
-    
+    path('health/', views.health_check, name='health_check'),  
 ]
 
